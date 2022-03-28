0eddc1451f238d400fd433d391ff0c827f65f623
OctavioSpano <tutispano@gmail.com>
2022-03-28 11:11:32 -0300

    28/3 ejs 3, 4 y 5

---
+++ Assets/Scripts/Ejercicio3.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio3 : MonoBehaviour
+{
+    // 3. Crear una variable pública que permita
+    //ingresar texto. Permitir que al ejecutar el 
+    //programa se muestre en la consola un mensaje 
+    //de saludo que incluya el texto de la variable.
+    //Por ejemplo, si en la variable se ingresa el texto 
+    //“Ana”, el mensaje en consola debe decir “Hola Ana”. 
+    public string nom;
+    void Start()
+    {
+        Debug.Log("Hola " + nom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ejercicio5.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio5 : MonoBehaviour
+{
+    //5. Realizá un programa que, dados dos números
+    //enteros num1 y num2 cargados desde  el Inspector
+    //muestre el resultado de la división del primero por 
+    //el segundo en la consola siguiendo el formato num1 + “ / “ 
+    //+ num2 + “ = “ + resultado. En el caso de que el segundo valor
+    //num2 sea cero, mostrar el siguiente mensaje de error: 
+    //“No se puede dividir por cero”. 
+    public int num1, num2;
+    void Start()
+    {
+        if (num2 == 0)
+        {
+            Debug.Log("No se puede dividir con cero");
+        }
+        else
+        {
+            Debug.Log(num1 + "/" + num2 + "=" + num1 /num2);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
